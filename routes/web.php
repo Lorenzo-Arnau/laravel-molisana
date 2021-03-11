f774c3c12b71e02a49b898042e92661cfd3e05eb
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-11 15:29:40 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/products/{id}', function($id) {
+    return view('products',
+        ['idProduct' => $id]
+    );
+});
+
